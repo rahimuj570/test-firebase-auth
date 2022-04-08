e9af2464fc5790e61a9f8a251506b7b6fddea8a9
rahimuj570 <rahimuj570@gmail.com>
2022-04-08 22:45:52 +0600

    successfully google auth added

--- src/Components/SuccessPage.js
+++ src/Components/SuccessPage.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
+import { getAuth, signOut } from "firebase/auth";
+import { auth } from "../firebase.int";
+import { useNavigate } from "react-router-dom";
 
 const SuccessPage = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  const signOutUser = () => {
+    signOut(auth)
+      .then(() => {
+        localStorage.setItem("user", JSON.stringify({}));
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const navigate = useNavigate();
+
   return (
     <div className=" my-3 text-center">
       {user.photoURL && <img src={user.photoURL} alt="" />}
 
       <h1>
         Congrats
-        <span className="fw-bold text-primary">{user.displayName}</span>
+        <span className="fw-bold text-primary"> {user.displayName}</span>
       </h1>
       <h2 className="text-uppercase">you are logged in </h2>
       {user.email && (
@@ -30,6 +45,12 @@ const SuccessPage = () => {
         Your User ID is
         <span className="fw-bold text-success"> {user.uid}</span>
       </h4>
+      <button
+        onClick={signOutUser}
+        className="btn bg-danger text-white mt-3 fw-bold"
+      >
+        Log Out
+      </button>
     </div>
   );
 };
