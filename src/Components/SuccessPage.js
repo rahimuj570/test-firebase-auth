cb46f1592069c3839148ca13ffc793313930ff86
rahimuj570 <rahimuj570@gmail.com>
2022-04-08 22:37:05 +0600

    successfull page designed

--- src/Components/SuccessPage.js
+++ src/Components/SuccessPage.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SuccessPage = () => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
   return (
-    <div>
-      <h1>Congrats NAme!!!</h1>
-      <h2>you are logged in </h2>
+    <div className=" my-3 text-center">
+      {user.photoURL && <img src={user.photoURL} alt="" />}
+
+      <h1>
+        Congrats
+        <span className="fw-bold text-primary">{user.displayName}</span>
+      </h1>
+      <h2 className="text-uppercase">you are logged in </h2>
+      {user.email && (
+        <h4>
+          Your Email is
+          <span className="fw-bold text-success">
+            {" "}
+            {user.emailVerified ? "Verified" : "Not Verified"}
+          </span>
+        </h4>
+      )}
+      {user.email && (
+        <h4>
+          Your Email is
+          <span className="fw-bold text-success"> {user.email}</span>
+        </h4>
+      )}
+      <h4>
+        Your User ID is
+        <span className="fw-bold text-success"> {user.uid}</span>
+      </h4>
     </div>
   );
 };
